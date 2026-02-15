93367c8f93914c8a3a1c751ce7c1a9b6aabbd457
notikke <notikke@vk.com>
2026-02-15 12:14:57 +0300

    Add landing and shared projects

---
+++ frontend/shared/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="shared-playground">
+      <h1>Hibinka Shared Components</h1>
+      <p>Здесь можно тестировать общие компоненты (кнопки, инпуты и т.д.)</p>
+
+      {/* В будущем сюда можно импортировать и добавлять компоненты для проверки */}
+    </div>
+  );
+}
+
+export default App;
