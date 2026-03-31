f090c5eeda8782f867c5faa9bd31884737aed136
Grigoriy <gakombarov@edu.hse.ru>
2026-03-31 20:53:54 +0300

    import fix

--- hibinka51-server/app/database.py
+++ hibinka51-server/app/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 from sqlalchemy.orm import DeclarativeBase
-from app.config import settings
+from app.core.config import settings
 
 engine = create_async_engine(settings.DATABASE_URL, echo=True)
 
